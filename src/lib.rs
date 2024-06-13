12dd40806cb697f0e54963cc5d4695c6a2ebb56f
prestance1 <oudrhiri.amin@gmail.com>
2024-06-13 19:34:37 +0100

    add stepdown rule

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,29 @@ use std::{
     cmp::min,
     collections::{BTreeMap, HashMap, VecDeque},
     error::Error,
+    fmt::Display,
 };
-
 
 #[derive(Debug, Copy, Clone)]
 pub enum LivreError {
     UnfillableOrder,
     OrderNotFound,
     DuplicateOrderId,
+    QuantityTooBig,
 }
+
+impl Display for LivreError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            LivreError::UnfillableOrder => write!(f, "Could not fill order"),
+            LivreError::DuplicateOrderId => write!(f, "Order id already in use"),
+            LivreError::QuantityTooBig => write!(f, "Fill quantity exceeds order lot size"),
+            LivreError::OrderNotFound => write!(f, "Could not find order matching id"),
+        }
+    }
+}
+
+impl Error for LivreError {}
 
 #[derive(Debug, Clone, Copy)]
 pub enum OrderType {
@@ -27,7 +41,12 @@ pub enum Side {
     Ask,
 }
 
-
+#[derive(Debug, Clone, Copy)]
+pub enum OrderState {
+    Filled,
+    PartialFill(u64),
+    Unfilled,
+}
 
 #[derive(Debug)]
 pub struct Order {
@@ -65,73 +84,20 @@ impl Order {
         if self.initial_quantity == self.remaining_quantity {
             OrderState::Unfilled
         } else if self.is_filled() {
-            OrderState::Filled    
+            OrderState::Filled
         } else {
             OrderState::PartialFill(self.initial_quantity - self.remaining_quantity)
         }
     }
 
-    pub fn fill(&mut self, quantity: u64) -> Result<(), ()> {
+    pub fn fill(&mut self, quantity: u64) -> Result<(), LivreError> {
         if quantity > self.remaining_quantity {
-            Err(())
+            Err(LivreError::QuantityTooBig)
         } else {
             self.remaining_quantity -= quantity;
             Ok(())
         }
     }
-}
-
-pub struct Trade {
-    pub taker_order_id: u64,
-    pub maker_order_id: u64,
-    pub price: u64,
-    pub quantity: u64,
-}
-
-impl Trade {
-    fn new(taker_order_id: u64, maker_order_id: u64, price: u64, quantity: u64) -> Self {
-        Self {
-            taker_order_id,
-            maker_order_id,
-            price,
-            quantity
-        }
-    }
-}
-
-
-type TradeLog = Vec<Trade>;
-pub struct LevelIdentifier {
-    price: u64,
-    side: Side,
-}
-#[derive(Debug, Clone, Copy)]
-pub enum OrderState {
-    Filled,
-    PartialFill(u64),
-    Unfilled,
-}
-
-
-pub struct MatchInfo {
-    pub trade_log: TradeLog,
-    pub order_state: OrderState,
-}
-
-impl MatchInfo {
-    fn new(trade_log: TradeLog, order_state: OrderState) -> Self {
-        Self {
-            trade_log,
-            order_state
-        }
-    }
-}
-
-pub struct Orderbook {
-    pub bids: BTreeMap<u64, VecDeque<Order>>,
-    pub asks: BTreeMap<u64, VecDeque<Order>>,
-    pub orders: HashMap<u64, LevelIdentifier>,
-    // pub orders: HashMap<u64, Order>
 }
 
 #[derive(Debug)]
@@ -153,6 +119,54 @@ impl ModifyOrder {
         )
     }
 }
+pub struct Trade {
+    pub taker_order_id: u64,
+    pub maker_order_id: u64,
+    pub price: u64,
+    pub quantity: u64,
+}
+
+impl Trade {
+    fn new(taker_order_id: u64, maker_order_id: u64, price: u64, quantity: u64) -> Self {
+        Self {
+            taker_order_id,
+            maker_order_id,
+            price,
+            quantity,
+        }
+    }
+}
+
+type TradeLog = Vec<Trade>;
+pub struct LevelIdentifier {
+    price: u64,
+    side: Side,
+}
+
+impl LevelIdentifier {
+    fn new(price: u64, side: Side) -> Self {
+        Self { price, side }
+    }
+}
+
+pub struct MatchInfo {
+    pub trade_log: TradeLog,
+    pub order_state: OrderState,
+}
+
+impl MatchInfo {
+    fn new(trade_log: TradeLog, order_state: OrderState) -> Self {
+        Self {
+            trade_log,
+            order_state,
+        }
+    }
+}
+pub struct Orderbook {
+    pub bids: BTreeMap<u64, VecDeque<Order>>,
+    pub asks: BTreeMap<u64, VecDeque<Order>>,
+    pub orders: HashMap<u64, LevelIdentifier>,
+}
 
 impl Orderbook {
     pub fn new() -> Self {
@@ -163,6 +177,62 @@ impl Orderbook {
         }
     }
 
+    pub fn add_order(&mut self, mut order: Order) -> Result<MatchInfo, LivreError> {
+        match order {
+            Order {
+                side,
+                price,
+                order_type: OrderType::FillAndKill,
+                ..
+            } => {
+                if !self.can_match(side, price) {
+                    return Err(LivreError::UnfillableOrder);
+                }
+            }
+
+            Order {
+                side,
+                price,
+                initial_quantity,
+                order_type: OrderType::FillOrKill,
+                ..
+            } => {
+                if !self.can_fully_fill(price, initial_quantity, side) {
+                    return Err(LivreError::UnfillableOrder);
+                }
+            }
+
+            Order { order_id, .. } if self.orders.contains_key(&order_id) => {
+                return Err(LivreError::DuplicateOrderId)
+            }
+            _ => {}
+        };
+
+        let trade_log = self.match_order(&mut order);
+        let order_state = order.order_state();
+        if !order.is_filled()
+            && matches!(
+                order.order_type,
+                OrderType::GoodForDay | OrderType::Market | OrderType::GoodTillCancel
+            )
+        {
+            self.orders.insert(
+                order.order_id,
+                LevelIdentifier::new(order.price, order.side),
+            );
+            let book_side = match order.side {
+                Side::Ask => &mut self.asks,
+                Side::Bid => &mut self.bids,
+            };
+            book_side
+                .entry(order.price)
+                .or_insert_with(VecDeque::new)
+                .push_back(order);
+        }
+
+        Ok(MatchInfo::new(trade_log, order_state))
+    }
+
     pub fn cancel_order(&mut self, order_id: u64) -> Result<Order, LivreError> {
         if let Some(level) = self.orders.get(&order_id) {
             let book_side = match level.side {
@@ -171,14 +241,84 @@ impl Orderbook {
             };
 
             let level = book_side.get_mut(&level.price).unwrap();
+            // can can unwrap as we know it exists in our orders map
             let idx = level
                 .iter()
                 .position(|order| order.order_id == order_id)
                 .unwrap();
-            self.orders.remove(&order_id).ok_or(LivreError::OrderNotFound)?;
+            self.orders
+                .remove(&order_id)
+                .ok_or(LivreError::OrderNotFound)?;
             level.remove(idx).ok_or(LivreError::OrderNotFound)
         } else {
             Err(LivreError::OrderNotFound)
+        }
+    }
+
+    pub fn modify_order(&mut self, order: ModifyOrder) -> Result<MatchInfo, LivreError> {
+        let old_order = self.cancel_order(order.order_id)?;
+        let order = order.to_order(old_order.order_type);
+        self.add_order(order)
+    }
+
+    pub fn order_count(&self) -> usize {
+        self.orders.len()
+    }
+
+    fn match_order(&mut self, order: &mut Order) -> TradeLog {
+        let mut trade_log = Vec::new();
+
+        match order.side {
+            Side::Bid => {
+                while let Some((best_price, queue)) = self.asks.pop_first() {
+                    if best_price > order.price {
+                        self.asks.insert(best_price, queue);
+                        break;
+                    }
+                    self.match_level(best_price, queue, order, &mut trade_log);
+                }
+            }
+            Side::Ask => {
+                while let Some((best_price, queue)) = self.bids.pop_last() {
+                    if best_price < order.price {
+                        self.bids.insert(best_price, queue);
+                        break;
+                    }
+                    self.match_level(best_price, queue, order, &mut trade_log);
+                }
+            }
+        };
+        trade_log
+    }
+
+    fn match_level(
+        &mut self,
+        best_price: u64,
+        mut queue: VecDeque<Order>,
+        order: &mut Order,
+        trade_log: &mut TradeLog,
+    ) {
+        while !order.is_filled() && !queue.is_empty() {
+            // already checked if queue is empty, so there will always be a front element
+            let maker_order = queue.front_mut().unwrap();
+            let trade_quantity = min(maker_order.remaining_quantity, order.remaining_quantity);
+            // can unwrap as quantity will necessarily be leq than both order's quantity
+            maker_order.fill(trade_quantity).unwrap();
+            order.fill(trade_quantity).unwrap();
+            trade_log.push(Trade::new(
+                order.order_id,
+                maker_order.order_id,
+                best_price,
+                trade_quantity,
+            ));
+            if maker_order.is_filled() {
+                self.orders.remove(&maker_order.order_id);
+                queue.pop_front();
+            }
+        }
+
+        if !queue.is_empty() {
+            self.asks.insert(best_price, queue);
         }
     }
 
@@ -223,129 +363,5 @@ impl Orderbook {
             quantity -= level_quantity;
         }
         false
-    }
-
-    pub fn modify_order(&mut self, order: ModifyOrder) -> Result<MatchInfo, LivreError> {
-        let old_order = self.cancel_order(order.order_id)?;
-        let order = order.to_order(old_order.order_type);
-        self.add_order(order)
-    }
-
-    pub fn order_count(&self) -> usize {
-        self.orders.len()
-    }
-
-    pub fn add_order(&mut self, mut order: Order) -> Result<MatchInfo, LivreError> {
-        match order {
-            Order {
-                side,
-                price,
-                order_type: OrderType::FillAndKill,
-                ..
-            } => {
-                if !self.can_match(side, price) {
-                    return Err(LivreError::UnfillableOrder);
-                }
-            }
-
-            Order {
-                side,
-                price,
-                initial_quantity,
-                order_type: OrderType::FillOrKill,
-                ..
-            } => {
-                if !self.can_fully_fill(price, initial_quantity, side) {
-                    return Err(LivreError::UnfillableOrder);
-                }
-            }
-
-            Order { order_id, .. } if self.orders.contains_key(&order_id) => return Err(LivreError::DuplicateOrderId),
-            _ => {}
-        };
-
-        let trade_log = self.match_order(&mut order);
-        let order_state = order.order_state();
-        if !order.is_filled()
-            && matches!(
-                order.order_type,
-                OrderType::GoodForDay | OrderType::Market | OrderType::GoodTillCancel
-            )
-        {
-            self.orders.insert(
-                order.order_id,
-                LevelIdentifier {
-                    price: order.price,
-                    side: order.side,
-                },
-            );
-            let book_side = match order.side {
-                Side::Ask => &mut self.asks,
-                Side::Bid => &mut self.bids,
-            };
-            book_side
-                .entry(order.price)
-                .or_insert_with(VecDeque::new)
-                .push_back(order);
-        }
-        
-        Ok(MatchInfo::new(trade_log, order_state))
-    }
-
-    pub fn match_order(&mut self, order: &mut Order) -> TradeLog {
-        let mut trade_log = Vec::new();
-
-        match order.side {
-            Side::Bid => {
-                while let Some((best_price, queue)) = self.asks.pop_first() {
-                    if best_price > order.price {
-                        self.asks.insert(best_price, queue);
-                        break;
-                    }
-                    self.match_level(best_price, queue, order, &mut trade_log);
-                }
-            }
-            Side::Ask => {
-                // maybe try to find an imeplemtnation that uses last entry instead
-                while let Some((best_price, queue)) = self.bids.pop_last() {
-                    if best_price < order.price {
-                        self.bids.insert(best_price, queue);
-                        break;
-                    }
-                    self.match_level(best_price, queue, order, &mut trade_log);
-                }
-            }
-        };
-        // clean up empty levels
-        trade_log
-    }
-
-    fn match_level(
-        &mut self,
-        best_price: u64,
-        mut queue: VecDeque<Order>,
-        order: &mut Order,
-        trade_log: &mut TradeLog,
-    ) {
-        while !order.is_filled() && !queue.is_empty() {
-            let maker_order = queue.front_mut().unwrap();
-            let trade_quantity = min(maker_order.remaining_quantity, order.remaining_quantity);
-            maker_order.fill(trade_quantity);
-            order.fill(trade_quantity);
-            trade_log.push(Trade {
-                taker_order_id: order.order_id,
-                maker_order_id: maker_order.order_id,
-                price: best_price,
-                quantity: trade_quantity,
-            });
-            if maker_order.is_filled() {
-                self.orders.remove(&maker_order.order_id);
-                queue.pop_front();
-            }
-        }
-
-        if !queue.is_empty() {
-            self.asks.insert(best_price, queue);
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use livre::{Order, Orderbook};
+
 fn main() {
-    println!("Hello, world!");
+    let mut order_book = Orderbook::new();
+    let bid1 = Order::new(livre::OrderType::Market, 1, livre::Side::Bid, 150, 10);
+    if let Ok(inf) = order_book.add_order(bid1) {
+        println!("{:?}", inf.order_state);
+    }
+    let ask1 = Order::new(livre::OrderType::Market, 2, livre::Side::Ask, 150, 5);
+    match order_book.add_order(ask1) {
+        Ok(inf) => println!("{:?}", inf.order_state),
+        Err(err) => println!("{}", err),
+    }
 }
